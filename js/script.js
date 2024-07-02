afb98674400dda77b87399f25a347ff0413f3862
anjie_f <amfarmer2022@gmail.com>
2024-07-02 08:19:36 -0500

    Added event listener

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
-const cardExpandOnHover = document.querySelectorAll(".supervisor, .team-builder, .calculator, .karma");
 
-cardExpandOnHover.forEach(card => {
-    card.addEventListener("mouseover", function () {
-        console.log("Mouse over:", card); // Debugging log
-        card.classList.add("expand"); /*this part expands the card; reference css .expand*/
-    });
-    card.addEventListener("mouseout", function () {
-        console.log("Mouse out:", card); // Debugging log
-        card.classList.remove("expand"); /*this part returns the cards its original non expanded state*/
-    })
+let supervisorSection = document.getElementById("supervisor-section");
+let teamSection = document.getElementById("team-section");
+let karmaSection = document.getElementById("karma-section");
+let calculatorSection = document.getElementById("calculator-section");
+
+
+//transition for the divs
+supervisorSection.style.transition = "transform 0.5s!important";
+teamSection.style.transition = "transform 0.5s!important";
+karmaSection.style.transition = "transform 0.5s!important";
+calculatorSection.style.transition = "transform 0.5s!important";
+
+//mouseover for divs
+supervisorSection.addEventListener("mouseover", function () {
+    supervisorSection.style.transform = "scale(1.3)";
 });
+teamSection.addEventListener("mouseover", function () {
+   teamSection.style.transform = "scale(1.3)";
+});
+karmaSection.addEventListener("mouseover", function () {
+    karmaSection.style.transform = "scale(1.3)";
+});
+calculatorSection.addEventListener("mouseover", function () {
+    calculatorSection.style.transform = "scale(1.3)";
+});
+
+//mouseout for divs
+supervisorSection.addEventListener("mouseout", function () {
+    supervisorSection.style.transform = "scale(1)";
+});
+teamSection.addEventListener("mouseout", function () {
+    teamSection.style.transform = "scale(1)";
+});
+karmaSection.addEventListener("mouseout", function () {
+    karmaSection.style.transform = "scale(1)";
+});
+calculatorSection.addEventListener("mouseout", function () {
+    calculatorSection.style.transform = "scale(1)";
+});
+
